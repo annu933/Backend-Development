56247f30c7b6befe41f36a5d7ff3fdd459a5305a
annu933 <kashyapannu933@gmail.com>
2024-10-17 19:35:46 +0530

    pull

--- CRUD_EJS/app.js
+++ CRUD_EJS/app.js
@@ -2,12 +2,17 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.json);
+app.set('view engine','ejs')
+app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname,'public')));
 
 app.get("/",function(req,res){
-    res.send("This is APP js file"); 
+    res.render("index"); 
 });
 
-app.listen(3000);
+app.get("/read",function(req,res){
+    res.render("read"); 
+});
+
+app.listen(3001);
